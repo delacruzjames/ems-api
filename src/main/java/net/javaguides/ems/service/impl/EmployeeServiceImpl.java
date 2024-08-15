56c0c27e2e72d3a948150c4c4e3cc2dd52d49677
delacruzjames <delacruzjamesmartin@gmail.com>
2024-08-15 17:41:24 +0800

    Implement GET Employee REST API using id

--- src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.ems.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems.dto.EmployeeDto;
 import net.javaguides.ems.entity.Employee;
+import net.javaguides.ems.exception.ResourceNotFoundException;
 import net.javaguides.ems.mapper.EmployeeMapper;
 import net.javaguides.ems.repository.EmployeeRepository;
 import net.javaguides.ems.service.EmployeeService;
@@ -20,5 +21,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeID) {
+        Employee employee = employeeRepository.findById(employeeID)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee Not Found"));
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
